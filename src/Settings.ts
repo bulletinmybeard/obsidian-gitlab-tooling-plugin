fa623b56b4110545b7720db59711206aa74eb690
Robin Schulz <hello@rschu.me>
2024-02-13 14:18:38 +0100

    - `SettingsGenerator` class have been moved to `SettingsClass` and the class file has been removed
    - Settings items can now depend on other items and will remain hidden until the dependent settings item is enabled

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,9 +1,10 @@
-import { App, Plugin, PluginSettingTab } from 'obsidian'
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian'
 
-import { SettingsGenerator } from './SettingsGenerator'
+import { capitalizeFirstLetter, getType } from './Utils'
 
 export class GitlabToolingSettingTab extends PluginSettingTab {
-	plugin: Plugin
+	plugin: any
+	settingsArray: SettingItem[]
 
 	constructor(app: App, plugin: any) {
 		super(app, plugin)
@@ -18,66 +19,152 @@ export class GitlabToolingSettingTab extends PluginSettingTab {
 			text: 'GitLab Tooling Configuration'
 		})
 
-		new SettingsGenerator(this.plugin, containerEl)
-			.addSettings([
-				{
-					name: 'Gitlab instance URL',
-					desc: 'Use your own Gitlab instance instead of the public hosted Gitlab.',
-					type: 'text',
-					placeholder: 'https://gitlab.com',
-					settingKey: 'gitlabUrl',
-				},
-				{
-					name: 'Personal Access Token',
-					desc: 'Create a personal access token in your Gitlab account and enter it here.',
-					type: 'text',
-					placeholder: 'Token',
-					settingKey: 'gitlabToken',
-				},
-				{
-					name: 'Open Merge Requests',
-					desc: 'Only fetch open merge requests.',
-					type: 'toggle',
-					settingKey: 'openMergeRequestsOnly',
-				},
-				{
-					name: 'Compact Info Card',
-					desc: 'Show Badges instead of the default info card.',
-					type: 'toggle',
-					settingKey: 'compactInfoCard',
-				},
-				{
-					name: 'Compact Mode',
-					desc: 'Toggle between \'Full Details\' for more information or \'Compact Mode\' for a concise view.',
-					type: 'toggle',
-					settingKey: 'compactMode',
-				},
-				{
-					name: 'Enable Debug Logging',
-					desc: 'Toggle to enable detailed debug log messages for troubleshooting and development purposes.',
-					type: 'toggle',
-					settingKey: 'enableDebugLogging',
-				},
-				{
-					name: 'Enable auto-polling',
-					desc: 'If enabled it will poll the GitLab REST API resources periodically.',
-					type: 'toggle',
-					settingKey: 'enableAutoPolling',
-				},
-				{
-					name: 'Auto-polling interval',
-					desc: 'Interval (e.g., 5m, 20m, 1h, etc.) at which to poll GitLab for updates.',
-					type: 'text',
-					placeholder: '5m',
-					settingKey: 'autoPollingInterval',
-					dependsOn: 'enableAutoPolling'
-				},
-				{
-					name: 'Cache REST API responses',
-					desc: 'Useful if account is rate limited.',
-					type: 'toggle',
-					settingKey: 'cacheRestApiResponses',
-				},
-			])
+		this.settingsArray = [
+			{
+				name: 'GitLab Host URL',
+				desc: `Specify the URL of your GitLab instance if you're using a self-hosted version. Default is GitLab.com.`,
+				type: 'text',
+				placeholder: 'https://gitlab.com',
+				settingKey: 'gitlabUrl',
+			},
+			{
+				name: 'Personal Access Token',
+				desc: 'Enter your GitLab Personal Access Token for authentication. Required for accessing private projects or for increased rate limits.',
+				type: 'text',
+				placeholder: 'Your Personal Access Token',
+				settingKey: 'gitlabToken',
+			},
+			{
+				name: 'Custom API Headers',
+				desc: 'Define custom headers for API requests for advanced use cases.',
+				placeholder: 'e.g., X-Custom-Header: Value',
+				type: 'text',
+				settingKey: 'customApiHeaders',
+			},
+			{
+				name: 'Only pull Open Merge Requests',
+				desc: 'Enable to only include open merge requests in the fetched data.',
+				type: 'toggle',
+				settingKey: 'openMergeRequestsOnly',
+			},
+			{
+				name: 'Display Mode',
+				desc: 'Choose between displaying detailed info cards or compact badges for merge requests and issues',
+				type: 'toggle',
+				settingKey: 'compactInfoCard',
+			},
+			{
+				name: 'Compact View',
+				desc: `Switch to 'Compact View' for a summarized representation, or stay on 'Full Details' for comprehensive information.`,
+				type: 'toggle',
+				settingKey: 'compactMode',
+			},
+			{
+				name: 'Enable Auto-Polling',
+				desc: `Automatically poll GitLab for updates at specified intervals. Helps keep data up-to-date without manual refresh.`,
+				type: 'toggle',
+				settingKey: 'enableDebugLogging',
+			},
+			{
+				name: 'Polling Interval',
+				desc: `Set the frequency for auto-polling GitLab updates (e.g., '5m' for every 5 minutes, '1h' for hourly). Requires auto-polling to be enabled.`,
+				placeholder: 'e.g., 5m, 1h',
+				type: 'text',
+				settingKey: 'enableAutoPolling',
+			},
+			{
+				name: 'Cache API Responses',
+				desc: 'Enable caching of GitLab API responses to minimize rate limiting issues and improve performance.',
+				type: 'toggle',
+				settingKey: 'cacheRestApiResponses',
+			},
+			{
+				name: 'Maximum Display Items',
+				desc: 'Limit the number of items (merge requests, issues) displayed at once.',
+				placeholder: 'e.g., 10',
+				type: 'text',
+				settingKey: 'maxDisplayItems',
+			},
+			{
+				name: 'Custom Date Format',
+				desc: 'Specify the date format used for displaying dates within the plugin.',
+				placeholder: 'e.g., YYYY-MM-DD',
+				type: 'text',
+				settingKey: 'customDateFormat',
+			},
+			{
+				name: 'Display Assignees',
+				desc: 'Show assignees on merge requests and issues.',
+				type: 'toggle',
+				settingKey: 'displayAssignees',
+			},
+		]
+
+		this.addSettings()
+	}
+
+	/**
+	 * Adds a setting block to the settings page.
+	 * @param {SettingItem} setting
+	 * @return {void}
+	 */
+	addDynamicSetting = (setting: any): void => {
+
+		const newSetting: any = new Setting(this.containerEl)
+			.setName(setting.name)
+			.setDesc(setting.desc)
+
+		const componentType = `add${capitalizeFirstLetter(setting.type)}`
+		if (getType(newSetting[componentType]) === 'function') {
+			const component = newSetting[componentType]((comp: any) => {
+				if ('setDisabled' in comp && setting?.disabled) {
+					comp.setDisabled(setting.disabled)
+				}
+				if ('addOption' in comp && setting.type === 'dropdown' && setting?.options) {
+					comp.addOption('defaultValue', 'Default Value')
+					for (const option of setting.options) {
+						comp.addOption(option.value, option.text)
+					}
+				}
+				if ('setPlaceholder' in comp && setting?.placeholder) {
+					comp.setPlaceholder(setting.placeholder)
+				}
+				if ('setValue' in comp) {
+					comp.setValue(this.plugin.settings[setting?.settingKey])
+				}
+				if ('onChange' in comp) {
+					comp.onChange(async (value: any) => {
+						console.log(`[${setting.name}] ${setting.settingKey}`, value)
+						this.plugin.settings[setting.settingKey] = value
+						await this.plugin.saveSettings()
+						this.display()
+					})
+				}
+			})
+			component.settingEl.setAttribute('data-setting', setting.settingKey)
+		} else {
+			console.error(`Unsupported setting type: ${setting.type}`)
+		}
+	}
+
+	/**
+	 * Adds a setting instance to the settings container.
+	 * @return {void}
+	 */
+	addSettings(): void {
+		this.settingsArray
+			.forEach((setting: SettingItem) => {
+				/**
+				 * Adds a dynamic setting if the `setting` does not depend on another setting,
+				 * or if it does and the dependent setting is enabled.
+				 */
+				if (setting?.dependsOn) {
+					if (this.plugin.settings?.[setting.dependsOn]) {
+						this.addDynamicSetting(setting)
+					}
+				} else {
+					this.addDynamicSetting(setting)
+				}
+			})
 	}
 }
